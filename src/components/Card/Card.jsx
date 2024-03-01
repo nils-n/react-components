9cd1f2fdbac8e7e3e41d8207b3ba1bf45ee35d58
Nils <52067471+nils-n@users.noreply.github.com>
2024-03-01 23:03:03 +0000

    add hover state to Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,29 @@
 import { HiCloudUpload } from "react-icons/hi";
+import { useEffect, useState } from "react";
+import classNames from "classnames";
+
 export default function Card({ icon = <HiCloudUpload />, children }) {
+  const [hover, setHover] = useState(false);
+
+  let cardClasses = classNames({
+    card: true,
+    'card--hover': hover,
+  });
+
+  function toggleHover() {
+    setHover((prev) => !prev);
+  }
+
+  useEffect(() => {
+  }, [hover]);
+
   return (
     <>
-      <div className="card">
+      <div
+        className={cardClasses}
+        onMouseEnter={toggleHover}
+        onMouseLeave={toggleHover}
+      >
         {children} <div className="card--icon">{icon}</div>
       </div>
     </>
